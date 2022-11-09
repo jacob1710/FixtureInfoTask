bee1e6316e5feb27e514b74459c0c5a81f152020
Jacob Scase <jacob.scase@checkd.media>
2022-11-09 10:43:24 +0000

    Android fix, fix incorrect flavors,

--- android/app/src/main/kotlin/com/example/fixture_info_task/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/fixture_info_task/MainActivity.kt
@@ -1,6 +1,25 @@
 package com.example.fixture_info_task
 
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugins.GeneratedPluginRegistrant
+import io.flutter.view.FlutterMain
 
 class MainActivity: FlutterActivity() {
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        GeneratedPluginRegistrant.registerWith(flutterEngine);
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        MethodChannel(flutterEngine?.getDartExecutor()?.getBinaryMessenger()!!, "flavor").setMethodCallHandler { call, result -> // here is the error
+
+            result.success(BuildConfig.FLAVOR)
+
+        }
+    }
 }
